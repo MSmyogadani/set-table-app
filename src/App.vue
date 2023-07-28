6cb27cf47332d034e2156cff2d1820e40c8cf215
myogadani <takeshi.myogadani@mediaseek.co.jp>
2023-07-28 15:58:20 +0900

    横幅を調整

--- src/App.vue
+++ src/App.vue
@@ -86,12 +86,12 @@
 </script>
 
 <template>
-  <div class="container-fluid w-75 mx-auto text-center">
+  <div class="container-fluid mx-auto text-center" style="width: 90%;">
       <div class="top mt-5 text-center">
         <div class="d-md-flex d-grid gap-2 justify-content-center justify-content-lg-between mb-3 top-item-block">
           <div class="d-flex order-1 order-lg-1 justify-content-center d-grid gap-3">
-            <Button :btnName="btnTableAdd" @clickEvent="addTable"><i class="bi bi-plus-circle-fill ms-2"></i></Button>
-            <Button :btnName="btnTableDel" @clickEvent="delTable"><i class="bi bi-dash-circle-fill ms-2"></i></Button>
+            <Button :btnName="btnTableAdd" @clickEvent="addTable"><i class="bi bi-plus-circle-fill ms-1"></i></Button>
+            <Button :btnName="btnTableDel" @clickEvent="delTable"><i class="bi bi-dash-circle-fill ms-1"></i></Button>
           </div>
           <div class="form-check form-switch d-flex align-items-center p-0 order-2 order-lg-3">
             <input
